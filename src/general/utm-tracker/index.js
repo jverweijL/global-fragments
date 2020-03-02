75ed28f67be1f5ba5b86d76ac856b6c4b3cbd2b8
Jan Verweij <jan.verweij@liferay.com>
2020-03-02 16:24:40 +0100

    some improvements to the utm-tracker to hide in view mode

--- src/general/utm-tracker/index.js
+++ src/general/utm-tracker/index.js
@@ -4,7 +4,7 @@ var editing = !!document.getElementsByClassName('fragments-editor').length;
 
 if (editing) {
   console.log("edit mode");
-  $(fragmentElement).addClass("visible");
+  $(fragmentElement).removeClass("invisible");
 } else {
   var regex = /[\?&]utm_.+?=([\s-_\w_]+)/g;
   var utmParams = window.location.href.match(regex);
