59611204277538e9c44c11fd19fd194fccf04727
Jan Verweij <jan.verweij@liferay.com>
2020-03-02 16:14:41 +0100

    some improvements to the utm-tracker to hide in view mode

--- src/general/utm-tracker/index.js
+++ src/general/utm-tracker/index.js
@@ -1,8 +1,16 @@
-$(function() {
+var editing = !!document.getElementsByClassName('fragments-editor').length;
+
+//console.log("id: " + $(fragmentElement).attr("id"));
+
+if (editing) {
+  console.log("edit mode");
+  $(fragmentElement).addClass("visible");
+} else {
   var regex = /[\?&]utm_.+?=([\s-_\w_]+)/g;
   var utmParams = window.location.href.match(regex);
   for (i = 0; i < utmParams.length; i++) { 
     document.cookie = utmParams[i].substring(1);
   }
   console.log( "UTM ready!" );
-});
+  $(fragmentElement).addClass("invisible");
+}
